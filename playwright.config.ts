3b551902845cf890b264d4d942602e7cf1938f9f
Gabriel Martinica <gabmartinica@gmail.com>
2023-08-13 12:17:40 -0400

    Add sum test

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,6 +28,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+
   },
 
   /* Configure projects for major browsers */
@@ -35,11 +37,6 @@ export default defineConfig({
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
     },
 
     {
--- tests/input.test.ts
+++ tests/input.test.ts
@@ -1,5 +1,4 @@
 import { expect, test } from "@playwright/test";
-import exp from "constants";
 
 test("Username Interaction", async ({ page }) => {
     await page.goto("https://www.lambdatest.com/automation-demos");
@@ -7,5 +6,26 @@ test("Username Interaction", async ({ page }) => {
     await expect(usernameField).toHaveId("username");
     console.log(await usernameField.inputValue());
     await usernameField.type("GamaRocks");
-    await expect(usernameField).toHaveText("GamaRocks");
+    const value = await expect(usernameField).toHaveValue("GamaRocks");
+});
+
+test("Sum", async ({ page }) => {
+    await page.goto(
+        "https://www.lambdatest.com/selenium-playground/simple-form-demo"
+    );
+    const sumInput1 = page.locator("#sum1");
+    const sumInput2 = page.locator("#sum2");
+
+    const num1 = 111;
+    const num2 = 224;
+    const result = num1 + num2;
+
+    const getValuesBtn = page.locator("//button[text()='Get Sum']");
+    await sumInput1.type(`${num1}`);
+    await sumInput2.type(`${num2}`);
+
+    await getValuesBtn.click();
+
+    const sumResult = page.locator("#addmessage");
+    await expect(sumResult).toContainText(`${result}`);
 });
